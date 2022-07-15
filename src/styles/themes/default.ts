7f53189e7ea9ad0eb0583fc43fa3f1068cdef858
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-07-15 02:25:05 -0300

    initial settings

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  'primary-700': '#C47F17',
+  'primary-400': '#DBAC2C',
+  'primary-100': '#F1E9C9',
+
+  'secondary-700': '#4B2995',
+  'secondary-400': '#8047F8',
+  'secondary-100': '#EBE5F9',
+
+  'base-900': '#272221',
+  'base-800': '#403937',
+  'base-700': '#574F4D',
+  'base-600': '#8D8686',
+  'base-500': '#D7D5D5',
+  'base-400': '#E6E5E5',
+  'base-300': '#EDEDED',
+  'base-200': '#F3F2F2',
+  'base-100': '#FAFAFA',
+  'base-0': '#FFFFFF'
+}
