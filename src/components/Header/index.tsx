f2304aed77254c535aa3fe6feae248e7e3d6b807
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-07-15 04:34:39 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
+import CoffeLogo from '../../assets/coffee-logo.svg';
 import { Button } from "../Button";
-import { HeaderContainer } from "./styles";
+import { ButtonGroup, HeaderContainer } from "./styles";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <h1>Header</h1>
-      <Button size="lg" variant="outline" text="Porto Alegre, RS" icon={{name: 'MapPin', weight: 'fill'}} mainColor="purple" />
+      <img src={CoffeLogo} />
+      <ButtonGroup>
+        <Button size="md" variant="outline" text="Porto Alegre, RS" icon={{name: 'MapPin', weight: 'fill'}} mainColor="purple" />
+        <Button size="md" variant="outline" icon={{name: 'ShoppingCart', weight: 'fill'}} mainColor="yellow" />
+      </ButtonGroup>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.div`
+export const HeaderContainer = styled.header`
+  position: sticky;
+  top: 0;
+
+  padding: 2rem 0;
+
+  background: ${props => props.theme['base-100']};
   
+  z-index: 999;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  gap: .25rem;
+
+  button {
+    flex: 0;
+  }
+
+  button:first-child{
+    flex: 1;
+  }
+`
